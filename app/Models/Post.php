664452e2011d4e7084e269f5706c4b5d04c92c17
MehdiGrimault <mehdi.grimault@gmail.com>
2023-12-07 09:42:21 +0100

    adding relation between posts and users, and cascade delete for posts when we delete a user

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["content", "image"];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
 }
