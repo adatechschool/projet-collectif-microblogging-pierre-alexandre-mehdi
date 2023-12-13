cedc2a9e280765ec51e2c04ed6656ae1e5a2dd8d
MehdiGrimault <mehdi.grimault@gmail.com>
2023-12-13 15:38:58 +0100

    add delete button to posts

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -3,11 +3,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 dark:text-gray-100">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h1>Les dernières actualités</h1>
-
-                    @foreach ($posts as $post)
-                        <x-post :post="$post"></x-post>
-                    @endforeach
+                    <h1 class="mb-8 ">Les dernières actualités</h1>
+                    <div class="grid grid-cols-3 gap-6 ">
+                        @foreach ($posts as $post)
+                            <x-post :post="$post"></x-post>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
