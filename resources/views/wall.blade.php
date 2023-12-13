87408c14c05e28b0de8c2219538144b7d8ade9c2
MehdiGrimault <mehdi.grimault@gmail.com>
2023-12-13 16:00:19 +0000

    add some margin on wall page

--- resources/views/wall.blade.php
+++ resources/views/wall.blade.php
@@ -3,17 +3,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 dark:text-gray-100">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h1 class="text-3xl font-semibold mb-4 text-black dark:text-white">Wall of {{ $user->name }}</h1>
+                    <h1 class="text-3xl font-semibold mb-4 text-black dark:text-white">{{ $user->name }}</h1>
                     <!-- Section : Biographie -->
                     <h2 class="text-2xl font-semibold mb-4 text-black dark:text-white">{{ $user->biography }}</h2>
 
-                    {{-- @dump (Auth::check())
-                    @dump  (Auth::user()->id)
-                    @dump ($user->id) --}}
 
+                    <!-- Section : Formulaire pour ajouter un post -->
                     @if (Auth::check() && Auth::user()->id === $user->id)
-                        <p>Formulaire pour créer un nouveau post</p>
-                        <div class="container">
+                        <div class="container m-10">
                             @include('components.post-form')
                         </div>
                     @endif
