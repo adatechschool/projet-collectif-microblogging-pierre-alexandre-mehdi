b64fc9736218e2d4abe07653b14fbb9b3553e5b6
Pierregirard44 <girard_pierre@hotmail.fr>
2023-12-14 10:15:57 +0100

    first version of Raclavel

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -3,7 +3,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 dark:text-gray-100">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h1 class="mb-8 ">Les dernières actualités</h1>
+                    <h1 class="mb-8 text-3xl font-semibold">Cheesiest news of the wonder world of Raclette</h1>
                     <div class="grid grid-cols-3 gap-6 ">
                         @foreach ($posts as $post)
                             <x-post :post="$post"></x-post>
--- resources/views/wall.blade.php
+++ resources/views/wall.blade.php
@@ -3,9 +3,15 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 dark:text-gray-100">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h1 class="text-3xl font-semibold mb-4 text-black dark:text-white">{{ $user->name }}</h1>
+                    <div class="flex items-center">
+                        <img class="h-28 w-28 rounded-full" src="{{ $user->profilPicture }}" />
+                        <div class="ml-3">
+                            <a href="{{ route('wall', ['slug' => $user->slug]) }}"
+                                class=" text-3xl font-semibold antialiased block leading-tight text-black">{{ $user->name }}</a>
+                        </div>
+                    </div>
                     <!-- Section : Biographie -->
-                    <h2 class="text-2xl font-semibold mb-4 text-black dark:text-white">{{ $user->biography }}</h2>
+                    <h2 class="text-2xl ml-12 mt-4 mb-4 text-black dark:text-white">{{ $user->biography }}</h2>
 
 
                     <!-- Section : Formulaire pour ajouter un post -->
